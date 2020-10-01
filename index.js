aad05d657b96ad6503882067f4303369fc6e7ed6
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-10-01 15:04:55 -0300

    Fished Proof Of Concept

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const fs = require('fs');
+const { execSync: exec } = require('child_process');
 const chalk = require('chalk');
-const [,,search,target] = process.argv;
+const [,, search, target, ...commands] = process.argv;
 
-if(!fs.existsSync(target)) return;
-const content = fs.readFileSync(target, 'utf8');
+let content;
+
+if (target === '@') {
+    let command = commands
+        .map(e => /\s/.test(e) ? `"${e}"` : e)
+        .join(' ');
+    try {
+        content = exec(command, { encoding: 'utf8' });
+    } catch (err) {
+        console.error(`Error executing command ${command}:\n` + err.stdout);
+        return;
+    }
+} else if (!fs.existsSync(target)) {
+    return;
+}
+else {
+    content = fs.readFileSync(target, 'utf8');
+}
 
 content.split(/\r?\n/g).forEach(line => {
-	const regex = new RegExp(search, 'g');
-	if(regex.test(line)) console.log(line.replace(regex, item => chalk.redBright(item)));
+    const regex = new RegExp(search, 'g');
+    if (regex.test(line)) console.log(line.replace(regex, item => chalk.redBright(item)));
 });
